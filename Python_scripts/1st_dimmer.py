6fa579f7c9d3dbdc5b2da217a9503dd177944d98
root <giaco.mix1221@gmail.com>
2021-02-21 18:06:13 +0100

    First Commit - 2 basic scripts

---
+++ Python_scripts/1st_dimmer.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+""" Definitions """
+
+freq = 100
+pin = 18
+min = 1
+max = 99
+
+""" GPIO SETTINGS """
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+pwm = GPIO.PWM(pin, freq)
+
+""" STARTING ROUTINE """
+
+pwm.start(0)
+
+check = 0
+while (check == 0):
+	value = input("Enter value for dimmer\n range 1 - 99: (-1 to end)\n")
+	if (value == -1):
+		pwm.ChangeDutyCycle(1)
+		check = -1
+	elif (value < min) or (value > max):
+		print("Out of range! Acceptable values only in 1-99\n")
+	else:
+		print (value)
+		pwm.ChangeDutyCycle(value)
+		time.sleep(1) 
+
+pwm.stop()
+
+GPIO.cleanup()
+
+""" EXITING """
+print("Thankyou for using!\n")
---
+++ Python_scripts/2nd_dimmer.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+
+""" Definitions """
+
+freq = 100
+pin = 18
+min = 1
+max = 99
+
+""" GPIO SETTINGS """
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+pwm = GPIO.PWM(pin, freq)
+
+""" STARTING ROUTINE """
+
+pwm.start(0)
+
+""" DIMMING UP """
+for i in range(1,99): 
+	pwm.ChangeDutyCycle(i)
+	time.sleep(0.1)
+
+""" DIMMING DOWN """
+for i in range(1,99): 
+	value = 100 - i
+	pwm.ChangeDutyCycle(value)
+	time.sleep(0.1)
+
+""" FLASHING BOLTS """
+for i in range (0, 3):
+	pwm.ChangeDutyCycle(99)
+	time.sleep(0.05)
+	pwm.ChangeDutyCycle(1)
+	time.sleep(0.05)
+	pwm.ChangeDutyCycle(99)
+	time.sleep(0.05)
+	pwm.ChangeDutyCycle(1)
+	time.sleep(3)
+
+""" ENDING """
+
+pwm.stop()
+
+GPIO.cleanup()
+
+""" EXITING """
+print("Thankyou for using!\n")
