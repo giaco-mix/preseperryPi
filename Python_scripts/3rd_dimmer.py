f242494a1323f1d2308300f8bfda75007d86289f
Stefano Bianchi <79417424+biankss@users.noreply.github.com>
2021-02-24 22:32:44 +0100

    Update 3rd_dimmer.py

--- Python_scripts/3rd_dimmer.py
+++ Python_scripts/3rd_dimmer.py
@@ -1,32 +1,31 @@
-from gpiozero import PWMLED
+import wiringpi as piwiring
 import time
 
 """ Definitions """
 
 freq = 100
-pin = 18
+pin2 = 18
 min = 0
-max = 100
+max = 1024
 
 """ GPIO SETTINGS """
-pwm = PWMLED(pin)
+piwiring.wiringPiSetupGpio()
+piwiring.pinMode(pin2, 2)
 
 """ STARTING ROUTINE """
-
+piwiring.pwmWrite(pin2, 0)
 
 check = 0
 while (check == 0):
-	value = input("Enter value for dimmer\n range 1 - 99: (-1 to end)\n")
+	value = input("Enter value for dimmer\n range 0 - 1024: (-1 to end)\n")
 	if (value == -1):
-		pwm.value = 1
+		piwiring.pwmWrite(pin2, 0)
 		check = -1
-	elif (value == 0.0):
-		pwm.off
-	elif (value <= min) or (value >=  max):
+	elif (value < min) or (value >  max):
 		print("Out of range! Acceptable values only in 1-99\n")
 	else:
 		print (value)
-		pwm.value = value
+		piwiring.pwmWrite(pin2, value)
 		time.sleep(1) 
 
 
